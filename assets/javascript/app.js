cd2127fca0cf300da332342d49608c87d1bb9069
francisco <francisco.deppe@gmail.com>
2017-05-22 13:14:54 -0700

    first adds

---
+++ assets/javascript/app.js
@@ -0,0 +1,15 @@
+var questionOne = {
+	choices: ["one", "two","three",],
+	questions: "which is the number",
+	answer: "one",
+}
+var questionTwo = {
+	choices: ["one", "two","three",],
+	questions: "which is the number",
+	answer: "one",
+}
+var questionThree = {
+	choices: ["one", "two","three",],
+	questions: "which is the number",
+	answer: "one",
+}
