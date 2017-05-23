09a5e306bc9bd5216959dc76dd4de39cb95e4a30
francisco <francisco.deppe@gmail.com>
2017-05-23 13:27:44 -0700

    updates

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,15 +1,68 @@
-var questionOne = {
-	choices: ["one", "two","three",],
-	questions: "which is the number",
-	answer: "one",
+function One() {
+    var questionOne = {
+        choices: ["one", "two", "three", "four"],
+        questions: "which is the number",
+        answer: "one",
+    }
+
+    function shuffle(array) {
+        for (var i = 0; i < questionOne.choices.length; i++) {
+            index = Math.floor(Math.random() * i);
+            var temp = questionOne.choices[index];
+            questionOne.choices[index] = questionOne.choices[i];
+            questionOne.choices[i] = temp;
+        }
+    }
+
+
+
+    shuffle(questionOne.choices);
+    console.log(questionOne.choices);
+    for (var i = 0; i < questionOne.choices.length; i++) {
+        $("#choices").append("<br>" + questionOne.choices[i]);
+    }
 }
-var questionTwo = {
-	choices: ["one", "two","three",],
-	questions: "which is the number",
-	answer: "one",
-}
-var questionThree = {
-	choices: ["one", "two","three",],
-	questions: "which is the number",
-	answer: "one",
-}
+
+
+
+// var questionTwo = {
+//     choices: ["one", "two", "three", ],
+//     questions: "which is the number",
+//     answer: "one",
+// }
+//
+// function shuffle(array) {
+//     for (var i = 0; i < questionTwo.choices.length; i++) {
+//         index = Math.floor(Math.random() * i);
+//         var temp = questionTwo.choices[index];
+//         questionTwo.choices[index] = questionTwo.choices[i];
+//         questionTwo.choices[i] = temp;
+//     }
+// }
+//
+// shuffle(questionTwo.choices);
+// console.log(questionTwo.choices);
+// for (var i = 0; i < questionTwo.choices.length; i++) {
+// 	$("#choices").append("<br>" + questionTwo.choices[i]);
+// }
+//
+// var questionThree = {
+//     choices: ["one", "two", "three", ],
+//     questions: "which is the number",
+//     answer: "one",
+// }
+//
+// function shuffle(array) {
+//     for (var i = 0; i < questionThree.choices.length; i++) {
+//         index = Math.floor(Math.random() * i);
+//         var temp = questionThree.choices[index];
+//         questionThree.choices[index] = questionThree.choices[i];
+//         questionThree.choices[i] = temp;
+//     }
+// }
+//
+// shuffle(questionThree.choices);
+// console.log(questionThree.choices);
+// for (var i = 0; i < questionThree.choices.length; i++) {
+// 	$("#choices").append("<br>" + questionThree.choices[i]);
+// }
