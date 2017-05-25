0ab5abea9a5d466fa6607fe81d65632101181dc8
Francisco Deppe <fdeppe@LB.local>
2017-05-25 14:43:01 -0700

    making progress

--- assets/javascript/app.js
+++ assets/javascript/app.js
@@ -1,68 +1,80 @@
-function One() {
     var questionOne = {
-        choices: ["one", "two", "three", "four"],
-        questions: "which is the number",
-        answer: "one",
+			answer: "one",
+			choices: ["two", "three", "four"],
+      questions: "which is the number",
+
     }
+    $("#question").html(questionOne.questions)
 
     function shuffle(array) {
-        for (var i = 0; i < questionOne.choices.length; i++) {
-            index = Math.floor(Math.random() * i);
-            var temp = questionOne.choices[index];
-            questionOne.choices[index] = questionOne.choices[i];
-            questionOne.choices[i] = temp;
-        }
+			questionOne.choices.push(questionOne.answer)
+      for (var i = 0; i < questionOne.choices.length; i++) {
+        index = Math.floor(Math.random() * i);
+        var temp = questionOne.choices[index];
+        questionOne.choices[index] = questionOne.choices[i];
+        questionOne.choices[i] = temp;
+      }
     }
-
-
 
     shuffle(questionOne.choices);
     console.log(questionOne.choices);
     for (var i = 0; i < questionOne.choices.length; i++) {
-        $("#choices").append("<br>" + questionOne.choices[i]);
+      $("#choices").append("<p>" + questionOne.choices[i] + "</p>");
     }
-}
+
+
+    $("p").on("click", function() {
+			var select = $(this).text();
+			if (select === questionOne.answer) {
+				alert("match")
+			}
+			else {
+				alert("wrong")
+			}
+		})
 
 
 
-// var questionTwo = {
-//     choices: ["one", "two", "three", ],
-//     questions: "which is the number",
-//     answer: "one",
-// }
-//
-// function shuffle(array) {
-//     for (var i = 0; i < questionTwo.choices.length; i++) {
-//         index = Math.floor(Math.random() * i);
-//         var temp = questionTwo.choices[index];
-//         questionTwo.choices[index] = questionTwo.choices[i];
-//         questionTwo.choices[i] = temp;
-//     }
-// }
-//
-// shuffle(questionTwo.choices);
-// console.log(questionTwo.choices);
-// for (var i = 0; i < questionTwo.choices.length; i++) {
-// 	$("#choices").append("<br>" + questionTwo.choices[i]);
-// }
-//
-// var questionThree = {
-//     choices: ["one", "two", "three", ],
-//     questions: "which is the number",
-//     answer: "one",
-// }
-//
-// function shuffle(array) {
-//     for (var i = 0; i < questionThree.choices.length; i++) {
-//         index = Math.floor(Math.random() * i);
-//         var temp = questionThree.choices[index];
-//         questionThree.choices[index] = questionThree.choices[i];
-//         questionThree.choices[i] = temp;
-//     }
-// }
-//
-// shuffle(questionThree.choices);
-// console.log(questionThree.choices);
-// for (var i = 0; i < questionThree.choices.length; i++) {
-// 	$("#choices").append("<br>" + questionThree.choices[i]);
-// }
+
+
+    // var questionTwo = {
+    //     choices: ["one", "two", "three", ],
+    //     questions: "which is the number",
+    //     answer: "one",
+    // }
+    //
+    // function shuffle(array) {
+    //     for (var i = 0; i < questionTwo.choices.length; i++) {
+    //         index = Math.floor(Math.random() * i);
+    //         var temp = questionTwo.choices[index];
+    //         questionTwo.choices[index] = questionTwo.choices[i];
+    //         questionTwo.choices[i] = temp;
+    //     }
+    // }
+    //
+    // shuffle(questionTwo.choices);
+    // console.log(questionTwo.choices);
+    // for (var i = 0; i < questionTwo.choices.length; i++) {
+    // 	$("#choices").append("<br>" + questionTwo.choices[i]);
+    // }
+    //
+    // var questionThree = {
+    //     choices: ["one", "two", "three", ],
+    //     questions: "which is the number",
+    //     answer: "one",
+    // }
+    //
+    // function shuffle(array) {
+    //     for (var i = 0; i < questionThree.choices.length; i++) {
+    //         index = Math.floor(Math.random() * i);
+    //         var temp = questionThree.choices[index];
+    //         questionThree.choices[index] = questionThree.choices[i];
+    //         questionThree.choices[i] = temp;
+    //     }
+    // }
+    //
+    // shuffle(questionThree.choices);
+    // console.log(questionThree.choices);
+    // for (var i = 0; i < questionThree.choices.length; i++) {
+    // 	$("#choices").append("<br>" + questionThree.choices[i]);
+    // }
